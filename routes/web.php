85424a8abb6a6388ba078e7a5969026e5dc36200
Mac_Duong_Le <apple@MacBook-Air-cua-APPLE.local>
2020-10-29 16:38:39 +0700

    admin category & template admin

---
+++ routes/web.php
@@ -0,0 +1,29 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+// Backend routes
+Route::get('/logout', 'SuperAdminController@logout');
+Route::get('/admin', 'AdminController@index');
+Route::get('/dashboard','SuperAdminController@index');
+Route::post('/admin-dashboard', 'AdminController@dashboard');
+
+// Category related route
+Route::get('/all-category', 'CategoryController@all_category');
+Route::get('/add-category', 'CategoryController@index');
+Route::post('/save-category', 'CategoryController@save_category');
+Route::get('/edit-category/{category_id}', 'CategoryController@edit_category');
+Route::post('/update-category/{category_id}', 'CategoryController@update_category');
+Route::get('/delete-category/{category_id}', 'CategoryController@delete_category');
+Route::get('/unactive-category/{category_id}', 'CategoryController@unactive_category');
+Route::get('/active-category/{category_id}', 'CategoryController@active_category');
+
